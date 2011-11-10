5475d229b421f198a9a8f030700a9ab99920a050
Mark S. Rasmussen <mark@improve.dk>
2011-11-10 18:34:36 +0100

    Initial code for OMS GUI

---
+++ src/OrcaMDF.OMS/Main.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace OrcaMDF.OMS
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openDatabaseDialog = new System.Windows.Forms.OpenFileDialog();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.treeview = new System.Windows.Forms.TreeView();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.menuStrip1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(478, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.openToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem1
+			// 
+			this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+			this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem1.Size = new System.Drawing.Size(206, 22);
+			this.openToolStripMenuItem1.Text = "Open Database...";
+			this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(203, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// openDatabaseDialog
+			// 
+			this.openDatabaseDialog.AddExtension = false;
+			this.openDatabaseDialog.FileName = "openFileDialog1";
+			this.openDatabaseDialog.Filter = "SQL Server data files|*.mdf;*.ndf";
+			this.openDatabaseDialog.Multiselect = true;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 282);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(478, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.treeview);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 24);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 258);
+			this.panel1.TabIndex = 3;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(200, 24);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(3, 258);
+			this.splitter1.TabIndex = 4;
+			this.splitter1.TabStop = false;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.dataGridView1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(203, 24);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(275, 258);
+			this.panel2.TabIndex = 5;
+			// 
+			// treeview
+			// 
+			this.treeview.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeview.Location = new System.Drawing.Point(0, 0);
+			this.treeview.Name = "treeview";
+			this.treeview.Size = new System.Drawing.Size(200, 258);
+			this.treeview.TabIndex = 0;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(275, 258);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(478, 304);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.splitter1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Main";
+			this.Text = "OrcaMDF Management Studio";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.OpenFileDialog openDatabaseDialog;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TreeView treeview;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ src/OrcaMDF.OMS/Main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OrcaMDF.Core.Engine;
+
+namespace OrcaMDF.OMS
+{
+	public partial class Main : Form
+	{
+		private Database db;
+
+		public Main()
+		{
+			InitializeComponent();
+
+			Disposed += Main_Disposed;
+		}
+
+		void Main_Disposed(object sender, EventArgs e)
+		{
+			if (db != null)
+				db.Dispose();
+		}
+
+		private void dumpException(Exception ex)
+		{
+			File.WriteAllText("ErrorLog.txt",
+				DateTime.Now +
+				Environment.NewLine +
+				"----------" +
+				Environment.NewLine +
+				ex +
+				Environment.NewLine);
+		}
+
+		private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			var result = openDatabaseDialog.ShowDialog();
+
+			if(result == DialogResult.OK)
+			{
+				try
+				{
+					var files = openDatabaseDialog.FileNames;
+					db = new Database(files);
+
+					refreshTreeview();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Please send the ErrorLog.txt file, from the application directory, to mark@improve.dk. Make sure to verify it does not contain any sensitive information before you send it.", "An error occurred while trying to open the database.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+					dumpException(ex);
+				}
+			}
+		}
+
+		private void refreshTreeview()
+		{
+			var rootNode = new TreeNode(db.Name);
+
+			// Add tables
+			var tableRootNode = rootNode.Nodes.Add("Tables");
+			var tables = db.Dmvs.Tables.OrderBy(t => t.Name);
+
+			foreach (var t in tables)
+			{
+				var tableNode = tableRootNode.Nodes.Add(t.Name);
+
+				// Add columns
+				var tableColumnsNode = tableNode.Nodes.Add("Columns");
+				var columns = db.Dmvs.Columns
+					.Where(c => c.ObjectID == t.ObjectID)
+					.OrderBy(c => c.Name);
+
+				foreach (var c in columns)
+					tableColumnsNode.Nodes.Add(c.Name);
+
+				// Add indexes
+				var tableIndexesNode = tableNode.Nodes.Add("Indexes");
+				var indexes = db.Dmvs.Indexes
+					.Where(i => i.ObjectID == t.ObjectID && i.IndexID > 0)
+					.OrderBy(i => i.Name);
+
+				foreach (var i in indexes)
+					tableIndexesNode.Nodes.Add(i.Name);
+			}
+
+			// Refresh treeview
+			treeview.Nodes.Clear();
+			treeview.Nodes.Add(rootNode);
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+	}
+}
