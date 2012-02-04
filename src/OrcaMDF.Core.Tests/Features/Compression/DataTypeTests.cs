650649f86709c855e8587789dbc1954523eb55df
Mark S. Rasmussen <mark@improve.dk>
2012-02-04 16:56:42 +0100

    Added support for compressed binary data type

---
+++ src/OrcaMDF.Core.Tests/Features/Compression/DataTypeTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+using NUnit.Framework;
+using OrcaMDF.Core.Engine;
+using OrcaMDF.Core.Tests.SqlServerVersion;
+
+namespace OrcaMDF.Core.Tests.Features.Compression
+{
+	public class DataTypeTests : SqlServerSystemTestBase
+	{
+		[SqlServer2008PlusTest]
+		public void BinaryTests(DatabaseVersion version)
+		{
+			RunDatabaseTest(version, db =>
+			{
+				var scanner = new DataScanner(db);
+				var rows = scanner.ScanTable("BinaryTest").ToList();
+
+				Assert.AreEqual(null, rows[0].Field<byte[]>("A"));
+				Assert.AreEqual(TestHelper.GetBytesFromByteString("25FF2500000000000000"), rows[1].Field<byte[]>("A"));
+				Assert.AreEqual(TestHelper.GetBytesFromByteString("01020304050607080910"), rows[2].Field<byte[]>("A"));
+			});
+		}
+
+		protected override void RunSetupQueries(SqlConnection conn, DatabaseVersion version)
+		{
+			RunQuery(@"
+				CREATE TABLE BinaryTest (A binary(10)) WITH (DATA_COMPRESSION = ROW)
+				INSERT INTO
+					BinaryTest
+				VALUES
+					(NULL),
+					(0x25FF25),
+					(0x01020304050607080910)
+				", conn);
+		}
+	}
+}
--- src/OrcaMDF.Core/Engine/SqlTypes/SqlBinary.cs
+++ src/OrcaMDF.Core/Engine/SqlTypes/SqlBinary.cs
@@ -24,10 +24,28 @@ namespace OrcaMDF.Core.Engine.SqlTypes
 
 		public override object GetValue(byte[] value)
 		{
-			if (value.Length != length)
-				throw new ArgumentException("Invalid value length: " + value.Length);
+			if (CompressionContext.CompressionLevel != CompressionLevel.None)
+			{
+				if (value.Length > length)
+					throw new ArgumentException("Invalid value length: " + value.Length);
 
-			return value;
+				if (value.Length < length)
+				{
+					var result = new byte[length];
+					Array.Copy(value, result, value.Length);
+
+					return result;
+				}
+				else
+					return value;
+			}
+			else
+			{
+				if (value.Length != length)
+					throw new ArgumentException("Invalid value length: " + value.Length);
+
+				return value;
+			}
 		}
 	}
 }
