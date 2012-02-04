63fb711453816991858e45d62390a3649c82d778
Mark S. Rasmussen <mark@improve.dk>
2012-02-04 20:28:09 +0100

    Added support for compressed decimals.

--- src/OrcaMDF.Core.Tests/Features/Compression/DataTypeTests.cs
+++ src/OrcaMDF.Core.Tests/Features/Compression/DataTypeTests.cs
@@ -58,6 +58,26 @@ namespace OrcaMDF.Core.Tests.Features.Compression
 				Assert.AreEqual("", rows[5].Field<string>("A"));
 			});
 		}
+
+		[SqlServer2008PlusTest]
+		public void DecimalTests(DatabaseVersion version)
+		{
+			RunDatabaseTest(version, db =>
+			{
+				var scanner = new DataScanner(db);
+				var rows = scanner.ScanTable("DecimalTests").ToList();
+
+				Assert.AreEqual(12.3m, rows[0].Field<decimal>("A"));
+				Assert.AreEqual(0m, rows[0].Field<decimal>("B"));
+				Assert.AreEqual(1m, rows[0].Field<decimal>("C"));
+				Assert.AreEqual(12345m, rows[0].Field<decimal>("D"));
+				Assert.AreEqual(39201.230m, rows[0].Field<decimal>("E"));
+				Assert.AreEqual(-4892384.382090m, rows[0].Field<decimal>("F"));
+				Assert.AreEqual(1328783742987.29m, rows[0].Field<decimal>("G"));
+				Assert.AreEqual(2940382040198493029.235m, rows[0].Field<decimal>("H"));
+				Assert.AreEqual(-1m, rows[0].Field<decimal>("I"));
+			});
+		}
 		
 		protected override void RunSetupQueries(SqlConnection conn, DatabaseVersion version)
 		{
@@ -94,6 +114,20 @@ namespace OrcaMDF.Core.Tests.Features.Compression
 					('123'),
 					(''),
 					(' ')
+
+				CREATE TABLE DecimalTests
+				(
+					A decimal(18, 4) NOT NULL,
+					B decimal(8, 0) NOT NULL,
+					C decimal(15, 7) NOT NULL,
+					D decimal(5, 0) NOT NULL,
+					E decimal(9, 3) NOT NULL,
+					F decimal(14, 6) NOT NULL,
+					G decimal(17, 2) NOT NULL,
+					H decimal(22, 3) NOT NULL,
+					I decimal(11, 3) NOT NULL
+				) WITH (DATA_COMPRESSION = ROW)
+				INSERT INTO DecimalTests VALUES (12.3, 0, 1, 12345, 39201.230, -4892384.38209, 1328783742987.29, 2940382040198493029.23456, -1)
 				", conn);
 		}
 	}
