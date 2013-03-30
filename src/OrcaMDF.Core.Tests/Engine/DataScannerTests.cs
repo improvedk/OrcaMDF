c00838d4171e4ee59f3c103c1fdba33d27a25932
Mark S. Rasmussen <mark@improve.dk>
2013-03-30 02:03:01 +0100

    Added a couple of failing tests

--- src/OrcaMDF.Core.Tests/Engine/DataScannerTests.cs
+++ src/OrcaMDF.Core.Tests/Engine/DataScannerTests.cs
@@ -42,6 +42,22 @@ namespace OrcaMDF.Core.Tests.Engine
 				Assert.AreEqual(1, rows[1].Field<int>(DataColumn.Uniquifier));
 			});
 		}
+
+		[SqlServerTest]
+		public void IgnoreDroppedColumnData(DatabaseVersion version)
+		{
+			RunDatabaseTest(version, db =>
+			{
+				var scanner = new DataScanner(db);
+				var rows = scanner.ScanTable("IgnoreDroppedColumnData").ToList();
+
+				Assert.AreEqual(1, rows.Count);
+				Assert.AreEqual(1, rows[0].Field<int>("A"));
+				Assert.AreEqual(27, rows[0].Field<int>("B"));
+				Assert.AreEqual("A", rows[0].Field<string>("C"));
+				Assert.AreEqual(3, rows[0].Field<int>("D"));
+			});
+		}
 		
 		protected override void RunSetupQueries(SqlConnection conn, DatabaseVersion version)
 		{
@@ -66,6 +82,26 @@ namespace OrcaMDF.Core.Tests.Engine
 
 						INSERT INTO NonUniqueClusteredTable (Num1, Name) VALUES (112, 'Doe')
 						INSERT INTO NonUniqueClusteredTable (Num1, Name) VALUES (112, 'Doe')", conn);
+			
+			// Create table with data, drop a few columns
+			RunQuery(@"	CREATE TABLE IgnoreDroppedColumnData
+						(
+							A int,
+							B int,
+							C varchar(30) NOT NULL,
+							D int
+						)
+
+						INSERT INTO
+							IgnoreDroppedColumnData (A, B, C, D)
+						VALUES
+							(1, 2, 'A', 3)
+						
+						ALTER TABLE IgnoreDroppedColumnData ALTER COLUMN A int NOT NULL
+						ALTER TABLE IgnoreDroppedColumnData ALTER COLUMN B int NOT NULL
+						ALTER TABLE IgnoreDroppedColumnData ALTER COLUMN D int NOT NULL
+						
+						UPDATE IgnoreDroppedColumnData SET B = 27", conn);
 		}
 	}
 }
