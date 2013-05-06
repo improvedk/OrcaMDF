4a5a530eeb7b45e31458936f632a2a471aa011e9
Justin Dearing <zippy1981@gmail.com>
2013-05-06 07:21:27 -0400

    Centralized assembly info to GlobalAssemblyInfo.cs

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Mark S. Rasmussen - improve.dk")]
+[assembly: AssemblyProduct("OrcaMDF.Core")]
+[assembly: AssemblyCopyright("Copyright © Mark S. Rasmussen 2011")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
