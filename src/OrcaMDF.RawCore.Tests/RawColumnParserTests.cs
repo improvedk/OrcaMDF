327bd905ec3420ddf3ed717c0efab352086ad801
Mark S. Rasmussen <mark@improve.dk>
2013-11-03 00:42:37 +0100

    Added test for SalesLT.Product

--- src/OrcaMDF.RawCore.Tests/RawColumnParserTests.cs
+++ src/OrcaMDF.RawCore.Tests/RawColumnParserTests.cs
@@ -138,7 +138,10 @@ namespace OrcaMDF.RawCore.Tests
 			Assert.AreEqual(Convert.ToDateTime(modifiedDate), result["ModifiedDate"]);
 		}
 
-		[TestCase(AW2005Path, 22, TestName = "2005")]
+		[TestCase(AW2005Path, 448, TestName = "2005")]
+		[TestCase(AW2008Path, 520, TestName = "2008")]
+		[TestCase(AW2008R2Path, 517, TestName = "2008R2")]
+		[TestCase(AW2012Path, 520, TestName = "2012")]
 		public void Parse_Product(string dbPath, int pageID)
 		{
 			var db = new RawDatabase(dbPath);
@@ -152,10 +155,37 @@ namespace OrcaMDF.RawCore.Tests
 				RawType.NVarchar("Color"),
 				RawType.Money("StandardCost"),
 				RawType.Money("ListPrice"),
-				RawType.NVarchar("Size")
+				RawType.NVarchar("Size"),
+				RawType.Decimal("Weight", 8, 2),
+				RawType.Int("ProductCategoryID"),
+				RawType.Int("ProductModelID"),
+				RawType.DateTime("SellStartDate"),
+				RawType.DateTime("SellEndDate"),
+				RawType.DateTime("DiscontinuedDate"),
+				RawType.VarBinary("ThumbNailPhoto"),
+				RawType.NVarchar("ThumbnailPhotoFileName"),
+				RawType.UniqueIdentifier("rowguid"),
+				RawType.DateTime("ModifiedDate")
 			});
 
-			Assert.AreEqual(5, result.Count);
+			Assert.AreEqual(17, result.Count);
+			Assert.AreEqual(680, result["ProductID"]);
+			Assert.AreEqual("HL Road Frame - Black, 58", result["Name"]);
+			Assert.AreEqual("FR-R92B-58", result["ProductNumber"]);
+			Assert.AreEqual("Black", result["Color"]);
+			Assert.AreEqual(1059.31, result["StandardCost"]);
+			Assert.AreEqual(1431.50, result["ListPrice"]);
+			Assert.AreEqual("58", result["Size"]);
+			Assert.AreEqual(1016.04, result["Weight"]);
+			Assert.AreEqual(18, result["ProductCategoryID"]);
+			Assert.AreEqual(6, result["ProductModelID"]);
+			Assert.AreEqual(Convert.ToDateTime("1998-06-01"), result["SellStartDate"]);
+			Assert.AreEqual(null, result["SellEndDate"]);
+			Assert.AreEqual(null, result["DiscontinuedDate"]);
+			Assert.AreEqual(1077, ((byte[])result["ThumbNailPhoto"]).Length);
+			Assert.AreEqual("no_image_available_small.gif", result["ThumbnailPhotoFileName"]);
+			Assert.AreEqual(new Guid("43dd68d6-14a4-461f-9069-55309d90ea7e"), result["rowguid"]);
+			Assert.AreEqual(Convert.ToDateTime("2004-03-11 10:01:36.827"), result["ModifiedDate"]);
 		}
 	}
 }
