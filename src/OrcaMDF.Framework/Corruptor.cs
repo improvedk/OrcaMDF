8fe58674faf810fc8cb02f48d5cd08afb1ee7378
Mark S. Rasmussen <mark@improve.dk>
2013-11-05 00:01:28 +0100

    Added Corruptor class to corrupt databases

---
+++ src/OrcaMDF.Framework/Corruptor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OrcaMDF.Framework
+{
+	public static class Corruptor
+	{
+		public static IEnumerable<int> CorruptFile(string path, double corruptionPercentage)
+		{
+			using (var file = File.OpenWrite(path))
+			{
+				var rnd = new Random();
+				byte[] zeros = new byte[8192];
+
+				int pageCount = (int)(file.Length / 8192);
+				int pageCountToCorrupt = (int)(pageCount * corruptionPercentage);
+				
+				IEnumerable<int> pageIDsToCorrupt = Enumerable
+					.Range(0, pageCount)
+					.OrderBy(x => rnd.Next())
+					.Take(pageCountToCorrupt)
+					.ToList();
+
+				foreach (int pageID in pageIDsToCorrupt)
+				{
+					file.Position = pageID * 8192;
+					file.Write(zeros, 0, 8192);
+				}
+
+				return pageIDsToCorrupt;
+			}
+		}
+	}
+}
