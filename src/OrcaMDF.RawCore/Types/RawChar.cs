e4d862cb426ebf17288d1360053746046e5948c3
Mark S. Rasmussen <mark@improve.dk>
2013-11-05 22:58:01 +0100

    Added support for the char data type

---
+++ src/OrcaMDF.RawCore/Types/RawChar.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace OrcaMDF.RawCore.Types
+{
+	public class RawChar : RawType, IRawFixedLengthType
+	{
+		public short Length { get; private set; }
+		
+		public RawChar(string name, short length) : base(name)
+		{
+			Length = length;
+		}
+
+		public override object GetValue(byte[] bytes)
+		{
+			return Encoding.ASCII.GetString(bytes);
+		}
+	}
+}
--- src/OrcaMDF.RawCore/Types/RawType.cs
+++ src/OrcaMDF.RawCore/Types/RawType.cs
@@ -10,6 +10,7 @@
 		}
 
 		public static RawBit Bit(string name) { return new RawBit(name); }
+		public static RawChar Char(string name, short length) { return new RawChar(name, length); }
 		public static RawDate Date(string name) { return new RawDate(name); }
 		public static RawDateTime DateTime(string name) { return new RawDateTime(name); }
 		public static RawDecimal Decimal(string name, byte precision, byte scale) { return new RawDecimal(name, precision, scale); }
