1ca203a082e3dbc1bd8ee82f89c7bf6d32bc44c0
Mark S. Rasmussen <mark@improve.dk>
2014-03-21 19:59:49 +0100

    Fully tested sysschobjs

--- src/OrcaMDF.RawCore.Tests/Utilities/SQL2012/SQL2012BaseSchemaTests.cs
+++ src/OrcaMDF.RawCore.Tests/Utilities/SQL2012/SQL2012BaseSchemaTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OrcaMDF.Framework;
 using OrcaMDF.RawCore.Records;
 using OrcaMDF.RawCore.Utilities.SQL2012;
+using System;
 using System.Linq;
 
 namespace OrcaMDF.RawCore.Tests.Utilities.SQL2012
@@ -22,6 +22,14 @@ namespace OrcaMDF.RawCore.Tests.Utilities.SQL2012
 			var testRow = rows.Single(x => (int)x["id"] == -93184835);
 			Assert.AreEqual("sp_helpsrvrolemember", testRow["name"]);
 			Assert.AreEqual(Convert.ToDateTime("2011-11-04 21:12:23.257"), testRow["created"]);
+
+			testRow = rows.Single(x => (int)x["id"] == 60);
+			Assert.AreEqual("sysobjvalues", testRow["name"]);
+			Assert.AreEqual(Convert.ToDateTime("2008-07-09 16:20:00.633"), testRow["modified"]);
+
+			testRow = rows.Single(x => (int)x["id"] == 1977058079);
+			Assert.AreEqual("QueryNotificationErrorsQueue", testRow["name"]);
+			Assert.AreEqual(Convert.ToDateTime("2008-07-09 16:20:00.913"), testRow["created"]);
 		}
 	}
 }
