c0f7a3e8c1a08c9a71a96204bc06c5d14da0996a
Mark S. Rasmussen <mark@improve.dk>
2014-03-23 00:45:25 +0100

    Added known SQL2012 system object: sys.syscolpars

--- src/OrcaMDF.RawCore/Utilities/SQL2012/SQL2012SystemObjects.cs
+++ src/OrcaMDF.RawCore/Utilities/SQL2012/SQL2012SystemObjects.cs
@@ -5,6 +5,31 @@ namespace OrcaMDF.RawCore.Utilities.SQL2012
 	public class SQL2012SystemObjects
 	{
 		public static SQL2012Sysschobjs sysschobjs = new SQL2012Sysschobjs();
+		public static SQL2012Syscolpars syscolpars = new SQL2012Syscolpars();
+	}
+
+	public class SQL2012Syscolpars
+	{
+		public int ObjectID = 41;
+
+		public IRawType[] Schema = {
+			RawType.Int("id"),
+			RawType.SmallInt("number"),
+			RawType.Int("colid"),
+			RawType.NVarchar("name"),
+			RawType.TinyInt("xtype"),
+			RawType.Int("utype"),
+			RawType.SmallInt("length"),
+			RawType.TinyInt("prec"),
+			RawType.TinyInt("scale"),
+			RawType.Int("collationid"),
+			RawType.Int("status"),
+			RawType.SmallInt("maxinrow"),
+			RawType.Int("xmlns"),
+			RawType.Int("dflt"),
+			RawType.Int("chk"),
+			RawType.VarBinary("idtval")
+		};
 	}
 
 	public class SQL2012Sysschobjs
