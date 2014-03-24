b4c01569e398a3077ce38c7355ed77e84b69ab52
Mark S. Rasmussen <mark@improve.dk>
2014-03-24 16:15:57 +0100

    Improved SQL2012Sysschobj tests

--- src/OrcaMDF.RawCore.Tests/Utilities/SQL2012/SQL2012SysschobjTests.cs
+++ src/OrcaMDF.RawCore.Tests/Utilities/SQL2012/SQL2012SysschobjTests.cs
@@ -1,24 +1,26 @@
 ﻿using NUnit.Framework;
 using OrcaMDF.Framework;
-using OrcaMDF.RawCore.Records;
 using OrcaMDF.RawCore.Utilities.SQL2012;
 using System;
 using System.Linq;
 
 namespace OrcaMDF.RawCore.Tests.Utilities.SQL2012
 {
-	public class SQL2012BaseSchemaTests : BaseFixture
+	public class SQL2012SysschobjTests : BaseFixture
 	{
 		[Test]
 		public void sysschobjs()
 		{
 			var db = new RawDataFile(AW2012Path);
 			var pages = db.Pages.Where(x => x.Header.ObjectID == SQL2012Sysschobjs.ObjectID && x.Header.Type == PageType.Data);
-			var records = pages.SelectMany(x => x.Records).Cast<RawPrimaryRecord>();
-			var rows = RawColumnParser.Parse(records, SQL2012Sysschobjs.Schema).ToList();
+			var rows = RawColumnParser.Parse(pages, SQL2012Sysschobjs.Schema).Select(SQL2012Sysschobjs.Row).ToList();
 
-			Assert.AreEqual(2291, rows.Count());
+			// Aggregates
+			Assert.AreEqual(2291, rows.Count);
+			Assert.AreEqual(48296, (int)rows.Average(x => x.status));
+			Assert.AreEqual(-394768470, (long)rows.Average(x => x.id));
 
+			// Individual rows
 			var testRow = rows.Single(x => x.id == -93184835);
 			Assert.AreEqual("sp_helpsrvrolemember", testRow.name);
 			Assert.AreEqual(Convert.ToDateTime("2011-11-04 21:12:23.257"), testRow.created);
