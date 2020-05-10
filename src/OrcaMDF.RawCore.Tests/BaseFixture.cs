c007ee811dad3c238a6afb62a45b17899dbe68d0
Brian Rogers <bobbymcr@gmail.com>
2020-05-10 16:24:22 -0700

    Support for .NET Standard 2.0 / .NET 4.6.1
    Add support for .NET Standard 2.0 (in all the core libraries) and
    .NET 4.6.1 (in the OMS and test projects).
    
    Summary of changes:
    
    - Update all projects to [SDK style][1]; set GenerateAssemblyInfo to false
      to [avoid having to touch existing AssemblyInfo files][2]
    - Use <PackageReference> instead of packages.config
    - Update .sln file to remove unnecessary build targets (assume AnyCPU)
    - Upgrade to newest NUnit
    - Modify path prefix in OrcaMDF.RawCore.Tests.BaseFixture
    - Move TestHelper out of core framework and include it as a linked file in
      all the test projects (this is needed because the code used here is
      [not compatible with .NET Standard][3])
    
    [1]: https://docs.microsoft.com/en-us/dotnet/core/tools/csproj
    [2]: https://github.com/dotnet/runtime/issues/26290
    [3]: https://stackoverflow.com/questions/42138418/equivalent-to-assemblyinfo-in-dotnet-core-csproj

--- src/OrcaMDF.RawCore.Tests/BaseFixture.cs
+++ src/OrcaMDF.RawCore.Tests/BaseFixture.cs
@@ -2,9 +2,11 @@
 {
 	public class BaseFixture
 	{
-		protected const string AW2012Path = @"TestDatabases\AWLT2012.mdf";
-		protected const string AW2008R2Path = @"TestDatabases\AWLT2008R2.mdf";
-		protected const string AW2008Path = @"TestDatabases\AWLT2008.mdf";
-		protected const string AW2005Path = @"TestDatabases\AWLT2005.mdf";
+		private const string Prefix = @"src\OrcaMDF.RawCore.Tests\TestDatabases\";
+
+		protected const string AW2012Path = Prefix + @"AWLT2012.mdf";
+		protected const string AW2008R2Path = Prefix + @"AWLT2008R2.mdf";
+		protected const string AW2008Path = Prefix + @"AWLT2008.mdf";
+		protected const string AW2005Path = Prefix + @"AWLT2005.mdf";
 	}
 }
